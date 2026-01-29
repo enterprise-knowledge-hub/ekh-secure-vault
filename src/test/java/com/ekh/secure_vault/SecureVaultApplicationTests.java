bed367c022d6ab07219acd7c472b6dd0cc71a66a
Lacky227 <tishko.artyr@gmail.com>
2026-01-29 22:45:58 +0200

    chore(vault): initialize project structure and tooling

---
+++ src/test/java/com/ekh/secure_vault/SecureVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekh.secure_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
